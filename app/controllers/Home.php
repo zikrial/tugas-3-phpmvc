5a0fbbda22882ff285243e72eae34bf638fce097
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-10-03 15:30:50 +0700

    Membuat template dari halaman

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,6 +3,9 @@
 class Home extends Controller{
     public function index()
     {
+        $data['judul'] = 'Home';
+        $this->view('templates/header', $data);
         $this->view('home/index');
+        $this->view('templates/footer');
     }
 }
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,13 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Home</title>
-</head>
-<body>
+
     <h1>Selamat Datang di Website Saya</h1>
-    
-</body>
-</html>
