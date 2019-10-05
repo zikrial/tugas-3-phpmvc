d1898df9578b3a38fefe1a448e0073d50a1f0abb
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-10-06 05:49:47 +0700

    Mengelola assets css, javascript dan gambar

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/tugas-3-phpmvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-    <h1>Halaman About</h1>
-
+<div class="container">
+    <h1 class = "mt-3">Halaman About</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Herapah" width="200" class="rounded-circle shadow">
     <p>Halo, nama Saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
