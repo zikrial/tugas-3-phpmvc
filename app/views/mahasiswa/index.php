55002afef9bf4f6867a5b536abc535808f1ac1b6
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-10-07 14:46:59 +0700

    Menambahkan fungsi untuk menghapus data Mahasiswa

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -16,9 +16,10 @@
             <h3>Daftar Mahasiswa</h3>
                 <ul class="list-group">
                     <?php foreach( $data['mhs'] as $mhs) : ?>
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <li class="list-group-item">
                             <?= $mhs['nama']; ?>
-                            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class = "badge badge-primary">detail</a>
+                            <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class = "badge badge-danger float-right ml-1" onclick="return confirm('Apakah anda yakin ingin menghapus data ini ?');">hapus</a>
+                            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class = "badge badge-primary float-right ml-1">detail</a>
                         </li>
                     <?php endforeach; ?>    
                 </ul>        
