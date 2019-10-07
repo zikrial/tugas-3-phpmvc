90e485e565d4de341fbb862d9f3f3644ea07db9a
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-10-07 13:21:20 +0700

    Membuat koneksi ke Database

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,25 +1,17 @@
 <?php
 
 class Mahasiswa_model {
-    private $dbh; //data base handler
-    private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=tugas-3-phpmvc';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch(PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->db = new Database;
     }
 
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
     }
 }
